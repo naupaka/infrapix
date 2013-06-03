051c47551f2163764b3626f0d91635c7137b1b25
Craig Wm. Versek <cversek+github@gmail.com>
2013-06-03 16:06:55 -0400

    basic template for python setuptools package distribution

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+"""   
+desc:  Setup script for 'infrapix' Python package and commandline tools.
+auth:  Pioneer Valley Open Science
+notes: Install with "python setup.py install".
+       Requires 
+"""
+import platform, os, shutil
+
+PACKAGE_METADATA = {
+    'name'         : 'infrapix',
+    'version'      : 'dev',
+    'author'       : "Pioneer Valley Open Science",
+    'author_email' : "https://github.com/Pioneer-Valley-Open-Science/infrapix/issues",
+}
+    
+PACKAGE_SOURCE_DIR = 'src'
+MAIN_PACKAGE_DIR   = 'infrapix'
+MAIN_PACKAGE_PATH  = os.path.abspath(os.sep.join((PACKAGE_SOURCE_DIR,MAIN_PACKAGE_DIR)))
+ 
+if __name__ == "__main__":
+    from setuptools import setup, find_packages    
+    setup(package_dir      = {'':PACKAGE_SOURCE_DIR},
+          packages         = find_packages(PACKAGE_SOURCE_DIR),
+          #non-code files
+          #package_data     =   {'': ['*.so']},
+          **PACKAGE_METADATA
+         )
+     
+
